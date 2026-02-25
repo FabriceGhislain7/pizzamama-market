c818d83df0f3ec01c47dfaec5da7e90afe063688
FabriceGhislain7 <ghislaintebou@gmail.com>
2026-02-25 03:11:56 +0100

    Homepage update.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,15 +13,27 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+URL configuration for config project.
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+# Root redirect verso Swagger Docs
+def homepage(request):
+    return redirect("/api/v1/docs/")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include("config.api_urls")),
+    path("", homepage),  # ROOT
+
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("config.api_urls")),
 ]
 
 
