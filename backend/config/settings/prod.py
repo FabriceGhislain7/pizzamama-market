494c98eceb6eefdf99b392176b812191b3b996a6
FabriceGhislain7 <ghislaintebou@gmail.com>
2026-02-27 22:55:02 +0100

    Progeto completato

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,3 +1,29 @@
 from .base import *
+import os
 
 DEBUG = False
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+if not SECRET_KEY:
+    raise Exception("DJANGO_SECRET_KEY not set in production.")
+
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.environ.get("ALLOWED_HOSTS", "").split(",")
+    if host.strip()
+]
+
+if not ALLOWED_HOSTS:
+    raise Exception("ALLOWED_HOSTS not configured in production.")
+
+
+# Security hardening
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
